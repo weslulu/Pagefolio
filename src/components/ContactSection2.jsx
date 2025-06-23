90e9d9e16d23fe7ee3216d80a53410bbf0f652c9
lumR19 <lamar.felemban@gmail.com>
2025-06-23 11:15:55 +0300

    contact us 2 (Responsive) is done

--- src/components/ContactSection2.jsx
+++ src/components/ContactSection2.jsx
@@ -12,42 +12,36 @@ const ContactSection2 = () => {
   return (
     <section>
       {/* صورة الزخرفة */}
-      <img
-        src={flagImage}
-        className="absolute left-0 right-0  w-full z-0"
-        alt="Decoration"
-      />
-
+      <img src={flagImage} className="absolute left-0 right-0  w-full z-0" alt="Decoration" />
+      
       {/* الخلفية الصفراء + الكروت + النص */}
-      <div className={`${background} px-4 md:px-15 sm:10 pt-23 z-10`}>
-        <div className="flex flex-col md:flex-row items-start justify-between gap-10">
-          
+      <div className={`${background} px-4 md:px-15 sm:px-10 pt-23 z-10`}>
+        <div className="flex flex-col md:flex-row-reverse  items-center justify-between gap-10">
+             {/* النص بعد الكروت (يمين) */}
+           <div className="text-white text-center text-2xl md:text-4xl sm:text-3xl   font-bold  leading-snug md:w-1/2">
+            <p>قم بزيارتنا للوصول و انضم الينا </p>
+
+          </div>  
           {/*  الكروت أول (يسار في الاتجاه العادي) */}
-          <div className="flex flex-col md:flex-row items-center gap-10 mt-7 md:mt-0">
+         <div className="flex flex-row items-center gap-10 md:mt-7 ">
             {maps.map((map, index) => (
               <div
                 key={index}
-                className="bg-white shadow-md overflow-hidden w-[290px] md:w-[350px] px-3 py-3"
+                className="bg-white shadow-md overflow-hidden  w-[180px] md:w-[270px] sm:w-[270px] px-3 py-3"
               >
                 <a href={map.link} target="_blank" rel="noopener noreferrer">
                   <img
                     src={map.image}
                     alt={`Map of ${map.city}`}
-                    className="w-full h-64 object-cover"
+                    className="w-full h-50 sm:h-60 md:h-64 object-cover"
                   />
                 </a>
-                <div className="text-right py-3 px-8 text-2xl font-bold text-[#0A5A7D]">
+                <div className="text-right py-3 px-8 text-lg md:text-2xl sm:text-2xl font-bold text-[#0A5A7D]">
                   {map.city}
                 </div>
               </div>
             ))}
-          </div>
-
-          {/* النص بعد الكروت (يمين) */}
-          <div className="text-white text-center text-xl md:text-4xl px-20 py-5 mt-25 font-bold  leading-snug md:w-1/2">
-            <p>قم بزيارتنا للوصول و انضم الينا </p>
-
-          </div>
+          </div> 
         </div>
       </div>
 
